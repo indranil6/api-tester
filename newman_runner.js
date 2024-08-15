63eb5abf95b52941e6d34626ab22dcf4c1194537
Indranil Kundu <indranil.kundu@clirnet.com>
2024-08-15 18:13:28 +0530

    updates

--- newman_runner.js
+++ newman_runner.js
@@ -1,38 +1,45 @@
-const newman = require('newman');
+const newman = require("newman");
 
-const postmanCollectionFile = 'postman_collection.json';
-const outputCsvFile = 'output.csv';
-const outputHtmlFile = 'output.html';
+const postmanCollectionFile = "postman_collection.json";
+const outputCsvFile = "output.csv";
+const outputHtmlFile = "output.html";
 
 async function runNewmanCollection() {
   return new Promise((resolve, reject) => {
-    newman.run({
-      collection: require(`./${postmanCollectionFile}`),
-      reporters: ['cli', 'csv', 'html', 'htmlextra', 'json'],
-      reporter: {
-        csv: {
-          export: outputCsvFile
+    newman.run(
+      {
+        collection: require(`./${postmanCollectionFile}`),
+        reporters: ["cli", "csv", "html", "htmlextra", "json"],
+        reporter: {
+          csv: {
+            export: outputCsvFile,
+          },
+          html: {
+            export: outputHtmlFile,
+          },
         },
-        html: {
-          export: outputHtmlFile
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
         }
       }
-    }, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
+    );
   });
 }
 
 async function main() {
   try {
-    await runNewmanCollection();
-    console.log(`Reports have been generated: ${outputCsvFile}, ${outputHtmlFile}`);
+    let response = await runNewmanCollection();
+
+    console.log(response);
+    console.log(
+      `Reports have been generated: ${outputCsvFile}, ${outputHtmlFile}`
+    );
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
 
